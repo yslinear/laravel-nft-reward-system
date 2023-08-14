4e626c79c4fc75ef08278694d65e91773402bd81
Ying-Shan Lin <yslinear@gmail.com>
2023-08-15 00:56:15 +0800

    新增 `user_daily_check_in_histories` table Migration 以及其 Model

---
+++ database/migrations/2023_08_14_164726_create_user_daily_check_in_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_daily_check_in_histories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_daily_check_in_histories');
+    }
+};
