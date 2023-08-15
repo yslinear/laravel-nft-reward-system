7feeb35a1fa4d8747c84f0d043ee6a237c189ae6
Ying-Shan Lin <yslinear@gmail.com>
2023-08-15 22:03:16 +0800

    新增 `products` table Migration 以及其 Model

---
+++ database/migrations/2023_08_15_135231_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('point');
+            $table->integer('nft_point');
+            $table->string('description');
+            $table->unsignedBigInteger('product_category_id');
+            $table->timestamps();
+
+            $table->foreign('product_category_id')->references('id')->on('product_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
