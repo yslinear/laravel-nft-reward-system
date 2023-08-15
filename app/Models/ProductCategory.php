c58c38b95429b9b8e6efa180417704d8162669d6
Ying-Shan Lin <yslinear@gmail.com>
2023-08-15 22:02:58 +0800

    新增 `product_categories` table Migration 以及其 Model

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'product_category_id');
+    }
+}
