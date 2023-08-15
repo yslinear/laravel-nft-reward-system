96a4fa9b6fbf1064baa2028ded7074b4bd5eba48
Ying-Shan Lin <yslinear@gmail.com>
2023-08-15 22:19:07 +0800

    新增 `product_user` table Migration 以及其 Model

---
+++ app/Models/ProductUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductUser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_user';
+
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'role_id',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
